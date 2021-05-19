47f94914ea6facb7e0450eea2f2915048baf78fd
Mike Wild <wildconsultingltd@googlemail.com>
2021-05-19 10:50:39 +0100

    Add project files.

---
+++ WebApplication3/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHealth()
+        {
+            //Call with http://localhost:5000/api/Health
+
+            return new OkObjectResult("All good");
+        }
+    }
+}
